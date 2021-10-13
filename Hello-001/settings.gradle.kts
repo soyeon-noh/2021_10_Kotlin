df001313e25a9daecf045105bd2a3acc2a4a0937
soyeon-noh <1010qwerty12@gmail.com>
2021-10-13 15:16:45 +0900

    reset

--- Hello-001/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-
-rootProject.name = "Hello-001"
-
--- Hello-001/src/main/java/Hello_01.java
+++
@@ -1,9 +0,0 @@
-public class Hello_01 {
-
-    public static void main(String[] args) {
-        System.out.printf("Hello Korea");
-
-
-    }
-
-}
